7b6cec254634dccd51ac19b1a6594da72bb3723c
Adam Warmack <awarmack@meaa.mea.com>
2018-08-14 13:25:31 -0400

    Finished Get Optimal VMC function. Now need to vectorize.

--- src/optimalPerformanceFunctions.R
+++ src/optimalPerformanceFunctions.R
@@ -47,12 +47,32 @@ getBTMvBTW <- function(btm, btw){
 }
 
 
+#convert bearing to +/- 0 (north) from 0~360
+convBearing <- function(bearing){
+  ifelse(bearing>180, bearing-360, bearing)
+}
+
+
+#convert bearing from +/-0 to 0~360
+convBearingBack <- function(bearing){
+  bearing <- ifelse(bearing>360, bearing-360, bearing)
+  bearing <- ifelse(bearing< -360, bearing+360, bearing)
+  bearing <- ifelse(bearing<0, bearing+360, bearing)
+  
+  return(bearing)
+}
+
+
+
 
 #figuring out max VMC Angle....
 # We'll check the VMC of both tacks seperately and decide if we're on the wrong tack elsewhere. 
+#note that this function is not vectorized.
 
 optvmc <- function(btm, twd, tws, pol.model){
   #given a bearing to the mark from wind  and vtw
+  
+  #twd = True Wind Direction (between 0 - 360)
 
   #create a vector of angles +/- from the btm
   twa <- seq(-179, 179, by=1)
@@ -60,27 +80,37 @@ optvmc <- function(btm, twd, tws, pol.model){
   #find all bsp on the polar
   v <- getOptV(abs(twa), tws, pol.model)  
   
+  
+  twd <- convBearing(twd)
+  
   #Add true wind direction to true wind angle to get actual bearing
-  bearings <- twd - twa
+  bearings <- twd + twa
+  
   #bearings <- ifelse(bearings <0, 360+bearings, bearings)
+  bearings <- ifelse(bearings > 360, bearings-360, bearings)
   
   #find difference between each bearing and the mark
-  btm <- ifelse(btm>180, btm-360, btm)
-  diff <- bearings - btm
+  btm <- ifelse(btm>180, btm-360, btm)  #convert to bearing +/- 180 degrees from north (rather than 0~360)
+  offmark <- bearings - btm   
   
-  vmc <- v*cos(diff*(pi/180))
+  vmc <- v*cos(offmark*(pi/180))
   
-  opt <- data.frame(twa, diff, bsp=v, vmc)
+  #return bearing to 0-360 formant
+  bearings <- convBearingBack(bearings)
+  
+
+  
+  opt <- data.frame(twd, twa, bearings, offmark, btm, bsp=v, vmc)
+  names(opt) <- c("twd", "opt_twa", "opt_bear", "DegOffMark", "btm", "bsp", "vmc")
   
   vmcopt <- max(vmc)  ### Could be multiple points (how to handle if on opp tacks?)
   
+  opt$vmc_gain <- vmcopt - opt$bsp[opt$DegOffMark==0]
+  
   #optimal angle to the mark
-  opttwa <- testtwa[which(vmcopt==vmc)]
+  return_opt <- opt[which(vmcopt==vmc), ]
   
-  #optimal boat speed at optimal vmc
-  bsp <- v[which(vmcopt==vmc)]
-  
-  return(data.frame(bsp=bsp, vmcopt=vmcopt, optangle=opttwa, bow=bow))
+  return(return_opt)
   
 }
 
