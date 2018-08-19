554b5501128bcbb66e6667ceef5db1de6da70b57
Adam Warmack <adam.warmack@gmail.com>
2018-08-19 16:14:26 -0400

    Complete Performance Calcs

--- src/calcPerformance.R
+++ src/calcPerformance.R
@@ -38,6 +38,7 @@ dat <- dat %>% filter(time <= finish_time)
 
 dat <- dat %>% distinct(boat, time, .keep_all=TRUE)
 
+dat <- dat %>% ungroup()
 
 
 ### Calculating Corrected Time for all boats ============================
@@ -50,8 +51,9 @@ dat$corr_elapsed <- as.numeric(dat$elapsed_secs) * as.numeric(dat$rating)
 
 dat$corr_time <- dat$corr_elapsed + dat$start_time
 
+posn <- dat
 
-
+save(posn, file="./data/positions_wcorr.Rdata")
 
 
 #Calculate the Zube's position vs. other boats ================================
@@ -93,12 +95,14 @@ zube_yb <- dat %>% filter(boat == "Zubenelgenubi")
 
 diff <- diff[diff$corr_time %in% zube_yb$corr_time, ]
 
-zube_yb$vsAlbacore <- diff$Albacore - diff$Zubenelgenubi
-zube_yb$vsBuffalo <- diff$`Flying Buffalo` - diff$Zubenelgenubi
-zube_yb$vsCourage <- diff$Courage - diff$Zubenelgenubi
-zube_yb$vsDefiant <- diff$Defiant - diff$Zubenelgenubi 
-zube_yb$vsJamJam <- diff$Jamjam - diff$Zubenelgenubi
-zube_yb$vsShillelagh <- diff$Shillelagh - diff$Zubenelgenubi
+zube_yb$vsAlbacore <- diff$Zubenelgenubi - diff$Albacore
+zube_yb$vsBuffalo <- diff$Zubenelgenubi - diff$`Flying Buffalo` 
+zube_yb$vsCourage <- diff$Zubenelgenubi - diff$Courage  
+zube_yb$vsDefiant <- diff$Zubenelgenubi - diff$Defiant 
+zube_yb$vsJamJam <- diff$Zubenelgenubi- diff$Jamjam 
+zube_yb$vsShillelagh <- diff$Zubenelgenubi - diff$Shillelagh
+
+save(zube_yb, file="./data/zube_yb_withdiff.rda")
 
 
 #Calculate Zube's performance ===========================================================
@@ -115,48 +119,81 @@ exp$time <- round_date(exp$time, unit="5 seconds")
 
 exp <- exp %>% group_by(time) %>% summarise_all(mean)
 
-#calculate performance
+
+#calculate performance to polars ========
 exp$opt_bsp <- getOptV(btw = abs(exp$twa), vtw = exp$tws, pol.model)
 
 exp$pol_perc <- (exp$sog / exp$opt_bsp) * 100
 
-#calculate optimal speed and angle for VMC. 
-#need Bearing to Mark.  To calculate, we need to know which mark we're on. 
-
-#join zube Yellowbrick position data and expedition performance data
-#discard duplicate times
-exp <- exp %>% distinct(time, .keep_all = TRUE) %>% select(-boat)
-
-zube <- full_join(zube_yb, exp, by="time", suffix=c(".yb", ".exp")) %>% arrange(time)
-
-#remove anything before the start
-zube <- zube %>% filter(time >= min(zube$start_time, na.rm=TRUE))
-
-#need to impute the mark information through out the entire data frame to calculate
-#carry the mark forward
-
-zube$mark <- na.locf(zube$mark)
-zube$markLat <- na.locf(zube$markLat)
-zube$markLon <- na.locf(zube$markLon)
-
-#bearing is in TRUE
-hasexppos <- !is.na(zube$lat.exp)
-
-p1 <- zube[hasexppos, c("lon.exp", "lat.exp")]
-p2 <- zube[hasexppos, c(c("markLon", "markLat"))]
-
-zube$btm[hasexppos] <- bearingRhumb(p1, p2)
-
-### Calculating the Vmc
-
-# get the bearing off the wind
-optimalcourse <- optvmc(btm = zube$btm, btw = zube$twa, vtw = zube$tws, pol.model = pol.model)
+exp$off_bsp <- exp$opt_bsp - exp$bsp
 
 
 
+#calculate optimal speed and angle for VMC =========
 
 
-#just looking at the time where we know both
-zube_yb_exp <- zube[zube$time %in% zube_yb$time, ]
+  
+  ##need Bearing to Mark.  To calculate, we need to know which mark we're on. 
+  
+    #join zube Yellowbrick position data and expedition performance data
+    #discard duplicate times
+    exp <- exp %>% distinct(time, .keep_all = TRUE) %>% select(-boat)
+    
+    zube <- full_join(zube_yb, exp, by="time", suffix=c(".yb", ".exp")) %>% arrange(time)
+  
+    #remove anything before the start
+    zube <- zube %>% filter(time >= min(zube$start_time, na.rm=TRUE))
+    
+    #need to impute the mark information through out the entire data frame to calculate
+    #carry the mark forward
+    zube$mark <- na.locf(zube$mark)    #locf = last observation carried forward
+    zube$markLat <- na.locf(zube$markLat)
+    zube$markLon <- na.locf(zube$markLon)
+  
+  ##clean up and impute missing positional data
+    ###Impute any TWD where we have TWA
+    zube$twd[!is.na(zube$tws)] <- getTWD(twa=zube$twa[!is.na(zube$tws)], hdg = zube$cog[!is.na(zube$tws)])
+  
+  
+    #Impute Expedition Lat/Lon by linear interpolation for any that have TWD
+    hastwd <- !is.na(zube$twd)
+    
+    zube$lat.exp[hastwd] <- apply(zube[hastwd, "lat.exp"], 2, FUN=imputeValues)
+    zube$lon.exp[hastwd] <- apply(zube[hastwd, "lon.exp"], 2, FUN=imputeValues)
+  
+  ##Calculate Bearing to the Mark
+  p1 <- zube[hastwd, c("lon.exp", "lat.exp")]
+  p2 <- zube[hastwd, c(c("markLon", "markLat"))]
+  
+  zube$btm[hastwd] <- bearingRhumb(p1, p2)
+  
+
+  # get the optimal bearing off the wind
+  optimalcourse <- mapply(FUN = optvmc, zube$btm, zube$twd, zube$tws, MoreArgs = list(pol.model), SIMPLIFY = FALSE)
+
+  optimalcourse <- do.call(bind_rows, optimalcourse)
+  
+  stopifnot(nrow(optimalcourse)==nrow(zube))
+
+  #add the optimal course back to the data frame
+  zube <- bind_cols(zube, optimalcourse[, 4:9])
+
+zube$act_vmc <- getVMC(btm = zube$btm, cog = zube$cog, bsp = zube$bsp)
+zube$off_vmc <- zube$opt_vmc - zube$act_vmc
+zube$vmc_perc <- (zube$act_vmc/zube$opt_vmc) * 100
+
+### Comparison of Performance vs. Position vs. Other Boats
+# First, we need to interpolate our position vs other boats since the points do not fully line up. 
+
+zube$vsAlbacore <- imputeValues(zube$vsAlbacore)
+
+zube$gain <- c(diff(zube$vsAlbacore) ,0) 
+zube$gainrate <- zube$gain / (as.numeric(c(diff(zube$time), 0))/3600)
+
+
+
+
+save(zube, file="./data/zube_performance.rda")
+
 
 
--- src/optimalPerformanceFunctions.R
+++ src/optimalPerformanceFunctions.R
@@ -16,12 +16,17 @@ matchLength <- function(v1, v2){
   return(list(v1,v2))
 }
 
-
+#get optimal boat speed at a given TWA (=btw)
 getOptV <- function(btw, vtw, pol.model){
+  
+  if(any(is.na(btw)) | any(is.na(vtw)) ){
+    return(NA)
+  }
+  
   #returns Optimal Velocity at a given Bearing to Wind (BTW) and Wind Speed (VTW)
   btw <- normbear(btw)
   
-  btw <- convBearing(btw)
+  btw <- fromNorth(btw)
 
   btw <- abs(btw)
     
@@ -38,75 +43,38 @@ getOptV <- function(btw, vtw, pol.model){
 }
 
 
-
-getBTMvBTW <- function(btm, btw){
-  #returns the difference between bearing to mark and wind
-  
-  diff <- btm-btw
-  
-  diff <- ifelse(diff > 180 | diff < -180, 360-diff, diff)
-
-  return(diff)
-    
-}
-
-
 #convert bearing to +/- 0 (north) from 0~360
 fromNorth <- function(bearing){
-  ifelse(bearing>180, bearing-360, bearing)
+  bearing <- normbear(bearing)  #first convert to a normal bearing
+  
+  fromNorth <- ifelse(bearing > 180, bearing-360, bearing)
+  fromNorth <- ifelse(fromNorth < -180, 360+fromNorth, fromNorth)
+  return(fromNorth)
+}
+
+normbear <- function(x){
+  #converts back to a normal bearing
+  x %% 360
 }
 
 
-#convert bearing from +/-0 to 0~360
-convBearingBack <- function(bearing){
-  bearing <- ifelse(bearing>360, bearing-360, bearing)
-  bearing <- ifelse(bearing< -360, bearing+360, bearing)
-  bearing <- ifelse(bearing<0, bearing+360, bearing)
+getTWA <- function(btm, twd){
+  #twd <- fromNorth(twd)
+  #btm <- fromNorth(btm)
   
-  return(bearing)
-}
-
-
-
-getDiffMarkWind <- function(btm, twd){
-  #gets the difference between the mark and bearing
-  # + = Stbd
-  # - = port
-  
-  twd <- convBearing(twd)
-  btm <- convBearing(btm)
-  
-  diffmark <- twd - btm
-  
-  return(diffmark)
+  fromNorth(twd-btm)
   
 }
 
-
-
-getOptVMC <- function(diffwind, tws, pol.model){
-  #variable = offmark distance between wind and mark
+getTWD <- function(twa, hdg){
+  #twa = TRUE WIND ANGLE
+  #hdg = Heading
+  hdg <- fromNorth(hdg)
+  twa <- fromNorth(twa)
   
-  twa <- seq(0,180, by=1)
-  
-  #test angles off the mark
-  offmark <- twa - diffwind
-  
-  #get the polar curve
-  v <- getOptV(twa, tws, pol.model)
-  
-  #vmc 
-  vmc <- v*cos(offmark*(pi/180))
-  
-  vmcMax <- max(vmc)
-  
-  opt <- data.frame(twa, offmark, v, vmc)
-  
-  return(opt[which(vmcMax == vmc), ])
-         
+  normbear(hdg + twa)
   
 }
-
 
 
 
@@ -115,46 +83,58 @@ getOptVMC <- function(diffwind, tws, pol.model){
 # We'll check the VMC of both tacks seperately and decide if we're on the wrong tack elsewhere. 
 #note that this function is not vectorized.
 
-optvmc <- function(btm, twd, tws, pol.model){
-  #given a bearing to the mark from wind  and vtw
-  
-  #twd = True Wind Direction (between 0 - 360)
+# optvmc <- function(btm, twd, tws, pol.model){
+#   #given a bearing to the mark from wind  and vtw
+#   
+#   #twd = True Wind Direction (between 0 - 360)
+# 
+#   #create a vector of angles +/- from the btm
+#   twa <- seq(-179, 179, by=1)
+#   
+#   #find all bsp on the polar
+#   v <- getOptV(twa, tws, pol.model)  
+#   
+#   #ensure TWD is in +/- 180 from 0. 
+#   twd <- convBearing(twd)
+#   
+#   #Add true wind direction to true wind angle to get actual bearings of each point of the polar
+#   bearings <- normbear(twd + twa)
+#   
+#   #find difference between each bearing and the mark
+#   btm <- convBearing(btm)  #convert to bearing +/- 180 degrees from north (rather than 0~360)
+#   offmark <- bearings - btm   
+#   
+#   vmc <- v*cos(offmark*(pi/180))
+#   
+#   #return bearing to 0-360 formant
+#   bearings <- convBearingBack(bearings)
+#   
+#   opt <- data.frame(btm, twd, twa, bearings, offmark, bsp=v, vmc)
+#   names(opt) <- c("btm", "twd", "opt_twa", "opt_bear", "DegOffMark", "opt_bsp", "vmc")
+#   
+#   vmcopt <- max(vmc)  ### Could be multiple points (how to handle if on opp tacks?)
+#   
+#   opt$vmc_gain <- vmcopt - getOptV(abs(btm-twd), vtw=tws, pol.model)
+#   
+#   #optimal angle to the mark
+#   return_opt <- opt[which(vmcopt==vmc), ]
+#   
+#   return(return_opt)
+#   
+# }
 
-  #create a vector of angles +/- from the btm
-  twa <- seq(-179, 179, by=1)
+getVMC <- function(btm, cog, bsp){
+  #given a bearing to the mark (BTM) and course over ground(COG)
+  # what is the vmc being made towards the mark
   
-  #find all bsp on the polar
-  v <- getOptV(twa, tws, pol.model)  
+  #get angle between btm and cog
+  angle <- fromNorth(cog) - fromNorth(btm)
   
-  #ensure TWD is in +/- 180 from 0. 
-  twd <- convBearing(twd)
+  vmc <- bsp * cos(angle*(pi/180))
   
-  #Add true wind direction to true wind angle to get actual bearings of each point of the polar
-  bearings <- normbear(twd + twa)
-  
-  #find difference between each bearing and the mark
-  btm <- convBearing(btm)  #convert to bearing +/- 180 degrees from north (rather than 0~360)
-  offmark <- bearings - btm   
-  
-  vmc <- v*cos(offmark*(pi/180))
-  
-  #return bearing to 0-360 formant
-  bearings <- convBearingBack(bearings)
-  
-  opt <- data.frame(btm, twd, twa, bearings, offmark, bsp=v, vmc)
-  names(opt) <- c("btm", "twd", "opt_twa", "opt_bear", "DegOffMark", "opt_bsp", "vmc")
-  
-  vmcopt <- max(vmc)  ### Could be multiple points (how to handle if on opp tacks?)
-  
-  opt$vmc_gain <- vmcopt - getOptV(abs(btm-twd), vtw=tws, pol.model)
-  
-  #optimal angle to the mark
-  return_opt <- opt[which(vmcopt==vmc), ]
-  
-  return(return_opt)
+  return(vmc)
   
 }
-
 
 
 optvmc <- function(btm, twd, tws, pol.model){
@@ -162,21 +142,50 @@ optvmc <- function(btm, twd, tws, pol.model){
   #TWD = True Wind Direction (0-360)
   #
   
+  col_names <-  c("btm", "twd", "tws", "opt_twa", "opt_bear", "degoffmark", "opt_bsp", "opt_vmc", "vmc_gain")
+  
+  if(any(is.na(c(btm, twd, tws)))){ 
+    
+    na_row <- as.data.frame(matrix(NA, nrow=1, ncol=9))
+    names(na_row) <- col_names
+    
+    return(na_row)
+  }
+  
+  
+  btm <- normbear(btm)
+  twd <- normbear(twd)
+  
   #nominal TWA (pointing at mark) where 0 at the wind
-  ntwa <- convBearing(btm) - convBearing(twd)
+  ntwa <- getTWA(btm, twd)
   
   #create vector of angles off the mark (0 at the wind)
-  off_mark <- seq(-180,180, by=1)
+  off_mark <- seq(-50,50, by=1)         #degrees away from the mark + = Stbd
+  twa_s <- fromNorth(ntwa + off_mark)   #twa at each degree off the mark
   
-  twa_s <- convBearing(ntwa + off_mark)   #twa at each degree off the mark
-    
-    
+  #bearings of each test position (actual direction of optimal VMC)
+  opt_bear <- normbear(btm - off_mark)
   
-  #bearings of each test position (actual bearings from the mark)
-  opt_bear <- btm + offmark
+  #get BSP at each twa
+  bsp <- getOptV(twa_s, vtw = tws, pol.model)
   
-  #get the TWA for each opt bearing (negative = port)
-  opt_twa <- fromNorth(twd) + fromNorth(opt_bear)
+  #get vmc at each angle off mark
+  vmc <- bsp * cos(off_mark*(pi/180))
+  
+  #find max VMC
+  vmcopt <- max(vmc)
+  
+  #find gain
+  if(abs(ntwa) < 40){  ntwa <- 40 }   #if we're on a beat, assume close hulled
+  
+  vmc_gain <- vmcopt - getOptV(ntwa, vtw=tws, pol.model)
+  
+  opt <- data.frame(btm, twd, tws, twa_s, opt_bear, off_mark, bsp, vmc, vmc_gain)
+  names(opt) <- col_names
+  
+  return_opt <- head(opt[which(vmcopt==vmc), ], 1)   #only return one value if there are more than 1
+  
+  return(return_opt)
   
 }
 
